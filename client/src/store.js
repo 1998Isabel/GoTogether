d62a86ed9a2b2136871922fde16eff1f81e562e1
1998isabel <1998isabel@gmail.com>
2019-12-31 17:17:50 +0800

    disable redux extention

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import { compose } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+// import { compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -11,13 +11,13 @@ const middleware  = [thunk];
 const store = createStore(
     rootReducer, 
     initialState, 
-    compose(
-       applyMiddleware(...middleware),
-       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-    // composeWithDevTools(
-    //     applyMiddleware(...middleware)
+    // compose(
+    //    applyMiddleware(...middleware),
+    //    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     // )
+    composeWithDevTools(
+        applyMiddleware(...middleware)
+    )
 );
 
 export default store;
